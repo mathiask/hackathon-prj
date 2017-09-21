c1be54e5638cdbeb289f0bd0736d846cb56a545d
Christian Zobel <christian.zobel@partner.bmw.de>
2017-09-21 19:16:08 +0200

    [CZO] added dummy implementation for rest webservice and configured servlet for correct mapping

--- src/main/java/com/capgemini/hackathon/rest/MyRestWebService.java
+++ src/main/java/com/capgemini/hackathon/rest/MyRestWebService.java
@@ -1,4 +1,22 @@
 package com.capgemini.hackathon.rest;
 
+import com.capgemini.hackathon.tech.logging.Log;
+import com.capgemini.hackathon.tech.logging.LogFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/myRestWebService")
 public class MyRestWebService {
+
+	private static final Log LOG = LogFactory.getLog();
+
+	@Path("/log")
+	@GET
+	@Produces("text/plain")
+	public String logMe() {
+		LOG.info("Logging");
+		return "TEST";
+	}
 }
