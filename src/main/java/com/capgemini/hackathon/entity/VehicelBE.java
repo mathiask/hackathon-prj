52b687f99fd348ff55b96552de64ce9cc3f6f535
Joachim Wenzel <joachim.wenzel@capgemini.com>
2017-09-26 10:32:04 +0200

    [JoWe] fixed be

--- src/main/java/com/capgemini/hackathon/entity/VehicelBE.java
+++ src/main/java/com/capgemini/hackathon/entity/VehicelBE.java
@@ -3,9 +3,12 @@ package com.capgemini.hackathon.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 @Entity
 @Table(name = "t_vehicle")
 public class VehicelBE {
@@ -16,4 +19,28 @@ public class VehicelBE {
     private String color;
 
     private int kw;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getKw() {
+        return kw;
+    }
+
+    public void setKw(int kw) {
+        this.kw = kw;
+    }
 }
